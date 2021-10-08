d0cd71330fe83956842d9d1685538ffab49675c7
Taeik Lim <sibera21@gmail.com>
2021-10-09 07:03:43 +0900

    Add before tasklet step example using bean

--- src/main/kotlin/acktsap/sample/step/taskletstep/bean/BatchApplication.kt
+++ src/main/kotlin/acktsap/sample/step/taskletstep/bean/BatchApplication.kt
@@ -2,10 +2,13 @@ package acktsap.sample.step.taskletstep.bean
 
 import org.springframework.batch.core.Job
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory
 import org.springframework.batch.core.step.tasklet.Tasklet
 import org.springframework.batch.extensions.kotlindsl.configuration.BatchDsl
 import org.springframework.batch.extensions.kotlindsl.configuration.EnableBatchDsl
 import org.springframework.batch.repeat.RepeatStatus
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
@@ -14,8 +17,28 @@ import org.springframework.context.annotation.Bean
 @EnableBatchProcessing
 @EnableBatchDsl
 class BatchApplication {
+    // common
+    @Bean
+    fun testTasklet(): Tasklet = Tasklet { _, _ ->
+        println("run testTasklet")
+        RepeatStatus.FINISHED
+    }
+
     // before
-    // none
+    @Bean
+    fun beforeJob(
+        jobBuilderFactory: JobBuilderFactory,
+        stepBuilderFactory: StepBuilderFactory,
+        @Qualifier("testTasklet") testTasklet: Tasklet
+    ): Job {
+        return jobBuilderFactory.get("beforeJob")
+            .start(
+                stepBuilderFactory.get("testStep")
+                    .tasklet(testTasklet)
+                    .build()
+            )
+            .build()
+    }
 
     // after
     @Bean
@@ -27,12 +50,6 @@ class BatchApplication {
                 }
             }
         }
-    }
-
-    @Bean
-    fun testTasklet(): Tasklet = Tasklet { _, _ ->
-        println("run testTasklet")
-        RepeatStatus.FINISHED
     }
 }
 
